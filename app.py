8684d468373e4e49c8363914993054929f5a6d1f
zakariabouchelaghem <z.bouchelaghem@esi-sba.dz>
2025-08-11 13:07:17 +0200

    deploy

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+from typing import Dict, Any
+import pandas as pd
+
+import tensorflow as tf 
+
+app=FastAPI()
+model = tf.keras.models.load_model("hand1_5_v3.h5")
+
+
+class ModelInput(BaseModel):
+     data: Dict[str, Any] 
+
+@app.post("/predict")
+
+def predict(input_data: ModelInput):
+    row = input_data.data
+    X = pd.DataFrame([row])
+    prediction = model.predict(X)[0]
+    return {"prediction": prediction}
+
+    
